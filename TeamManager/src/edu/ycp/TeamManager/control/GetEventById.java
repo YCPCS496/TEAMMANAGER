9d2b6187b68d459ded87cb9cb9a1906121907444
Dan Mashuda <dmashuda@ycp.edu>
2014-05-01 11:48:19 -0400

    Added GetEvent to version control

---
+++ TeamManager/src/edu/ycp/TeamManager/control/GetEventById.java
@@ -0,0 +1,14 @@
+package edu.ycp.TeamManager.control;
+
+import edu.ycp.TeamManager.Model.Event;
+import edu.ycp.TeamManager.persist.Database;
+import edu.ycp.TeamManager.persist.IDatabase;
+
+public class GetEventById {
+	public Event getEventById(String id){
+		IDatabase db = Database.getInstance();
+		return db.getEventById(id);
+
+	}
+
+}
